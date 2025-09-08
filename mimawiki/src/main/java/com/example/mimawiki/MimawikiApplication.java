293534f5c953549671f96062b0b041cec6e26460
민정원 <jeongone.min@gmail.com>
2025-09-08 11:45:03 +0900

    Add files via upload

---
+++ mimawiki/src/main/java/com/example/mimawiki/MimawikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mimawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimawikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimawikiApplication.class, args);
+	}
+
+}
